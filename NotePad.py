0cf5dd6336fa7b8b7a7fad4193f07e021e514324
KariHab <habboutk@outlook.com>
2023-06-25 20:48:02 -0400

    working

--- NotePad.py
+++ NotePad.py
@@ -6,15 +6,16 @@ import PySimpleGUI as sg
 
 sg.theme('Black')
 image = 'ToDo_list\TO-DO-list---List-Checker\icon.ico'
-t1 = sg.Input(visible=False, enable_events=True, key='-T1-', font=('Arial Bold', 10), expand_x=True)
-t2 = sg.Input(visible=False, enable_events=True, key='-T2-', font=('Arial Bold', 10), expand_x=True)
+t1 = sg.Input(visible=False, enable_events=True, key='-T1-', font=('Arial', 10), expand_x=True)
+t2 = sg.Input(visible=False, enable_events=True, key='-T2-', font=('Arial', 10), expand_x=True)
 t3 = sg.Multiline("", enable_events=True, key='-INPUT-', expand_x=True, expand_y=True, justification='left')
-layout = [[t3],
+layout = [[sg.Text("Drop your ideas below", font=('Arial Bold', 10))],
+    [t3],
     [[t1, sg.FilesBrowse(file_types=(("Text Files", "*.txt"),)),t2, sg.FileSaveAs(file_types=(("Text Files", "*.txt"),))]], 
 ]
 
 
-window = sg.Window('NotePad', layout, size=(600, 400), location=(0,625), icon=image)
+window = sg.Window('NotePad', layout, size=(600, 400), location=(0,625), icon = 'icon.png')
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED: 
